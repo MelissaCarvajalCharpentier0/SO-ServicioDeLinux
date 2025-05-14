85731b2e1cc7214ef75dd28187144ccb36e7b272
meli <meli@DebianMelissa.myguest.virtualbox.org>
2025-05-13 19:13:40 -0600

    Deploy update 2

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,12 @@ LOCAL=$(git rev-parse Deployment)
 REMOTE=$(git rev-parse origin/Deployment)
 
 if [ "$LOCAL" != "$REMOTE" ]; then
-    echo "[`date`] Updating Deployment..." >> deploy.log
-    git pull origin Deployment >> deploy.log
-    /bin/ systemctl restart app.service
+    echo "[$(date)] Updating Deployment..." >> deploy.log
+    git pull origin Deployment >> deploy.log 2>&1
+
+    echo 'tu_contraseña' | sudo -S systemctl restart app.service >> deploy.log 2>&1
+
+    echo "[$(date)] Deployment complete." >> deploy.log
 else
-    echo "[`date`] There is no changes on Deployment branch" >> deploy.log
+    echo "[$(date)] There is no changes on Deployment branch" >> deploy.log
 fi
